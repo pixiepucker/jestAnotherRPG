b2adfe3b3e1d3f24a0ed7cb769dfb85c64d7a857
pixiepucker <autumnoffic@gmail.com>
2022-04-10 12:43:57 -0500

    used protoype to create getStats and getInventory functions

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,4 +1,5 @@
-const { expect } = require('@jest/globals');
+const { test, expect } = require('@jest/globals');
+const { debugPort } = require('process');
 const Player = require('../lib/Player');
 const Potion = require('../lib/__mocks__/Potion');
 
@@ -14,4 +15,23 @@ test('creates a player object', ()=>{
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test('gets player stats as an object', ()=>{
+    const player = new Player('Maya');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', ()=>{
+    const player = new Player('Maya');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
